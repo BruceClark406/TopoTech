050aa6f27308ebe6cecf78a3498e4a9172fd54ac
BruceClark406 <brucewestonmt@gmail.com>
2019-12-21 23:41:57 -0700

    Object viewer

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ app.debug = True
 def index():
     return render_template('home.html')
 
+
+@app.route('/object_viewer')
+def object_viewer():
+    return render_template('object_viewer.html')
+
 if __name__ == "__main__":
     app.run()
