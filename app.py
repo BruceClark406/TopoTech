9b42c21e872dbfbc7360047f4a8872aae62348b0
BruceClark406 <brucewestonmt@gmail.com>
2019-12-21 21:37:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World! It"s Bruce coming at you from bozeman mt</h1><p>He is rooteness tooteness<p>'
+    return '<h1>It"s Bruce coming at you from bozeman mt</h1><p>He is rooteness tooteness<p>'
 
 # if __name__ == "__main__":
 #     app.run()
